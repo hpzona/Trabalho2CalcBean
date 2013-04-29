0361132d2342e66ca2a38a3a1feeb93449172110
Willian <Willian@Willian-PC>
2013-04-29 19:55:15 -0300

    primeira versão

--- Trabalho2CalcBean/src/trabalho2calcbean/Trabalho2CalcBean.java
+++ Trabalho2CalcBean/src/trabalho2calcbean/Trabalho2CalcBean.java
@@ -1,7 +1,13 @@
 package trabalho2calcbean;
+
 public class Trabalho2CalcBean {
 
     public static void main(String[] args) {
-
+        javax.swing.JFrame frame = new javax.swing.JFrame();  // Cria frame
+        CalcBean bean = new CalcBean(); // Instancia o bean
+        // Alterar as propriedades que julgar necessárias aqui!
+        frame.add(bean);  // Adiciona bean ao frame
+        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);  // Exibe o frame
     }
 }
